95bdc7c528c701997251f3b5152356f38fb896a6
Abnoan Muniz <abnoanmuniz@gmail.com>
2023-12-21 08:50:41 -0300

    Adjusting project organization

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ProxyServerBrightData
+﻿namespace WebScrapBrightData
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Bright Data Proxy Configuration
+            string host = "zproxy.lum-superproxy.io";
+            int port = 22225;
+            string username = "your_username";
+            string password = "your_password";
+            var client = BrightDataProxyConfigurator.ConfigureHttpClient(host, port, username, password);
+
+            // Scrape content from the target URL
+            string urlToScrape = "https://www.wikipedia.org/";
+            var content = await WebContentScraper.ScrapeContent(urlToScrape, client);
+
+            // Extract data from the HTML content
+            HtmlDataExtractor.ExtractHyperlinks(content);
         }
     }
 }
--- WebContentScraper.cs
+++ WebContentScraper.cs
@@ -1,14 +1,14 @@
 ﻿using HtmlAgilityPack;
 
-namespace BrightDataProxy
+namespace WebScrapBrightData
 {
     public class WebContentScraper
     {
         public static async Task ScrapeContent(string url, HttpClient client)
-        {         
+        {
             var response = await client.GetAsync(url);
             var content = await response.Content.ReadAsStringAsync();
-           
+
             HtmlDocument doc = new();
             doc.LoadHtml(content);
             var nodes = doc.DocumentNode.SelectNodes("//li/a[@href] | //p/a[@href] | //td/a[@href]");
@@ -19,11 +19,11 @@ namespace BrightDataProxy
                 {
                     string hrefValue = node.GetAttributeValue("href", string.Empty);
                     string title = node.InnerText;
-               
+
                     Uri baseUri = new(url);
                     Uri fullUri = new(baseUri, hrefValue);
 
-                    Console.WriteLine($"Title: {title}, Link: {fullUri.AbsoluteUri}");      
+                    Console.WriteLine($"Title: {title}, Link: {fullUri.AbsoluteUri}");
                 }
             }
             else
